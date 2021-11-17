5f02b77a6bc69252c69c1b705c89e3342205a60d
somasekhar1 <somasekhar0023@gmail.com>
2021-11-17 12:18:31 +0530

    host

--- chats/settings.py
+++ chats/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_mq&5+&ab*2+m^!8z^^3e6iha%i9sh)94#$7#3l8!_s287k##k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['privatechats.herokuapp.com']
 
 
 # Application definition
